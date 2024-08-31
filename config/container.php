66568c0b8e654d6f7761e0f40a51d9d371ce46b7
Luís Vinícius <vinicius1230@gmail.com>
2024-08-31 06:15:22 -0300

    feat: add queue config validation to stack

--- config/container.php
+++ config/container.php
@@ -4,6 +4,7 @@ use Bwyty\ServiceConfigValidator\Service\ConfigDataCatcher\ConfigDataCatcher;
 use Bwyty\ServiceConfigValidator\Service\ConfigDataCatcher\ConfigDataCatcherInterface;
 use Bwyty\ServiceConfigValidator\Service\RepoNameCatcher\RepoNameCatcher;
 use Bwyty\ServiceConfigValidator\Service\RepoNameCatcher\RepoNameCatcherInterface;
+use Bwyty\ServiceConfigValidator\Service\Validation\QueueConfigValidation;
 use Bwyty\ServiceConfigValidator\Service\Validation\ServiceNameValidation;
 use Bwyty\ServiceConfigValidator\ValidationStack;
 use DI\ContainerBuilder;
@@ -20,6 +21,7 @@ $container->set(ConfigDataCatcherInterface::class, DI\autowire(ConfigDataCatcher
 $container->set(ValidationStack::class, function (ContainerInterface $container) {
     $validationStack = new ValidationStack();
     $validationStack->addValidation($container->get(ServiceNameValidation::class));
+    $validationStack->addValidation($container->get(QueueConfigValidation::class));
     return $validationStack;
 });
 
